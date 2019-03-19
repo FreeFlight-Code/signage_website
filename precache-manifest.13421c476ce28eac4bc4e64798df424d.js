04707a557c96ca9c899b3826ddd94d8884f75118
David <david@dfischer.lan>
2019-03-19 08:37:37 -0600

    Updates

---
+++ precache-manifest.13421c476ce28eac4bc4e64798df424d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ebdd1cdae07214b468e",
+    "url": "/signage_website/static/js/runtime~main.b2bd9248.js"
+  },
+  {
+    "revision": "5ab43dc3b00b61513f51",
+    "url": "/signage_website/static/js/main.eb42f8da.chunk.js"
+  },
+  {
+    "revision": "aeb252a98df2389cb4c4",
+    "url": "/signage_website/static/js/2.b91bfe70.chunk.js"
+  },
+  {
+    "revision": "5ab43dc3b00b61513f51",
+    "url": "/signage_website/static/css/main.06a48df1.chunk.css"
+  },
+  {
+    "revision": "21ddd07bbe7f0c342c40a5088eca047a",
+    "url": "/signage_website/index.html"
+  }
+];

0c82e28aa713a46e3fe20dea84f941fef5e0488b
FreeFlight-Code <davidfisc@hotmail.com>
2019-07-23 00:49:46 -0600

    clean up css add finalsimustream video

--- src/Components/YTubeFrame.js
+++ src/Components/YTubeFrame.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 *Sample obj from ads
 *
 *   {
-*       name: "company_name",                                   // string name to maybe scroll or something in the future    
+*       name: "company_name",                                   // string name to maybe scroll or something in the future
 *       url: "www.youtube.com/embed/adsgfkjhsfa?autoplay=1",    // string string representing direct url to video
 *       adsPerDay: 3                                            // number for # of views in a day
 *       timeRequested: 1230                                     // number for military time
@@ -16,7 +16,7 @@ export default class YTubeFrame extends Component {
     constructor(props){
         super(props)
         this.state = {
-            defaultVideo: `https://www.youtube.com/embed?playlist&list=PLvFz1VAZcZYZWb3i7xlswGDTz2WJp3MLr&autoplay=1&index=5`,
+          defaultVideo: `https://final.simustream.com/watch/ryoulive/fd188ce355638986373ef230fbb28fd8`,
             currentVideo: ``
         }
     }
@@ -29,9 +29,9 @@ export default class YTubeFrame extends Component {
                 title='YouTube Player'
                 src={this.state.currentVideo || this.state.defaultVideo}
                 frameBorder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen>
             </iframe>
         )
     }
-} 
+}
